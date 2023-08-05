26bb94c979d0c2170de33b3da4c6ef4156709da0
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2023-08-05 18:12:40 -0300

    feat: create http adapter

--- lib/core/injection_manager.dart
+++ lib/core/injection_manager.dart
@@ -1,22 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_request_adapter/adapters/dio_adapter.dart';
+import 'package:flutter_request_adapter/adapters/http_adapter.dart';
 import 'package:flutter_request_adapter/adapters/request_client.dart';
 import 'package:flutter_request_adapter/core/injection.dart';
 import 'package:flutter_request_adapter/data/repositories/pokemon_repository_impl.dart';
 import 'package:flutter_request_adapter/domain/repositories/pokemon_repository.dart';
 import 'package:flutter_request_adapter/domain/usecase/get_single_pokemon.dart';
+import 'package:http/http.dart';
 
 class InjectionManager {
   void injectDependencies() {
     getIt.registerSingleton<RequestClient>(
-      DioAdapter(
-        dioInstace: Dio(
-          BaseOptions(
-            baseUrl: 'https://pokeapi.co/api/v2/',
-            connectTimeout: const Duration(seconds: 5),
-            receiveTimeout: const Duration(seconds: 3),
-          ),
-        ),
+      HttpAdapter(
+        client: Client(),
+        baseUrl: 'https://pokeapi.co/api/v2/',
       ),
     );
     getIt.registerFactory<PokemonRepository>(
